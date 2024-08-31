f1a4dcb1a04bdd00627b5dd3ef6b98ef8b3e6dde
Samuel Lucky <samuelnoye35@gmail.com>
2024-08-31 11:15:08 +0100

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 // routes/user.js
 const express = require('express');
-const { getUser, verifyPhone, deleteUser,requestOTPForgotPIN, updateUserName,verifyOTPForgotPIN, resetPIN} = require('../controllers/userController');
+const { getUser, verifyPhone, deleteUser,requestOTPForgotPIN, updateUserName,verifyOTPForgotPIN, resetPIN, } = require('../controllers/userController');
 const {verifyTokenAndAuthorization} = require('../middlewares/verifyToken')
 
 const router = express.Router();
@@ -11,6 +11,7 @@ router.put('/forgot-pin/reset', resetPIN);
 router.get("/", verifyTokenAndAuthorization, getUser);
 router.post('/verify-phone/:id/:otp', verifyPhone);
 router.put('/updateUserName/:id/:first_name/:last_name', verifyTokenAndAuthorization, updateUserName);
+router.delete('/deleteUser/:id', verifyTokenAndAuthorization, deleteUser);
 
 router.delete("/", verifyTokenAndAuthorization, deleteUser);
 
