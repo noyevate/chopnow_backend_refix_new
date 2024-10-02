bbfc6841ff3795736e088f2c3001f3220d5318a8
Samuel Lucky <samuelnoye35@gmail.com>
2024-10-02 13:27:23 +0100

    Create Additive.js

---
+++ models/Additive.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const AdditiveSchema = new mongoose.Schema({
+    userId: {type: String, required:true},
+    title: {type: String, required:true},
+    max: {type: Number, required:false},
+    min: {type: Number, required:false},
+    name: {type: String, required: true},
+    price: {type: Number, required:true},
+},{
+    toJSON: {
+        transform(doc, ret){
+            delete ret.__v;
+        }
+    },
+    timestamps: true
+});
+
+module.exports = mongoose.model('Additives', AdditiveSchema)
