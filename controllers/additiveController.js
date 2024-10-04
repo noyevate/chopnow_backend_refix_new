6b7ed3faf4be7d12aad97bab6ac89b2835e4d8ca
Samuel Lucky <samuelnoye35@gmail.com>
2024-10-04 05:05:57 +0100

    additive Controller

--- controllers/additiveController.js
+++ controllers/additiveController.js
@@ -1,3 +1,24 @@
 const Additive = require("../models/Additive");
 const Restaurant = require("../models/Restaurant");
 const mongoose = require('mongoose');
+
+async function addAdditive(req, res) {
+    const { restaurantId, name, price } = req.body;
+    if (restaurantId || name || price) {
+        return res.status(400).json({ status: false, message: "You have a missing field" });
+    } 
+
+    try {
+        // Add food
+        const newFood = new Additive(req.body);
+        await newAdditive.save();
+        
+
+        res.status(201).json({ status: true, message: "additive has been added successfully" });
+    } catch (error) {
+        res.status(500).json({ status: false, message: error.message });
+    }
+}
+
+
+module.exports = { addAdditive}
--- routes/additiveRoute.js
+++ routes/additiveRoute.js
@@ -1,3 +1,5 @@
 const router = require('express').Router();
 const additiveController = require('../controllers/addditiveController');
 const {verifyTokenAndAuthorization} = require('../middlewares/verifyToken')
+
+router.post("/", additiveController.addAdditive);
