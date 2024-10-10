a9f07910813dde999250f291b83bc2e498babf59
Samuel Lucky <samuelnoye35@gmail.com>
2024-10-10 13:06:28 +0100

    Update Additive.js

--- models/Additive.js
+++ models/Additive.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose');
 
 const AdditiveSchema = new mongoose.Schema({
     restaurantId: {type: String, required:true},
-    title: {type: String, required:true},
+    additiveTitle: {type: String, required:true},
+    additiveName: {type: String, required:true},
     max: {type: Number, required:false},
     min: {type: Number, required:false},
     name: {type: String, required: true},
     price: {type: Number, required:true},
+    foods: { type: Array, default: [] },
+    isAvailable: {type: Boolean, default:true},
 },{
     toJSON: {
         transform(doc, ret){
