45763af9cdffa72c9e4a1f1d560aadb642e6732c
Samuel Lucky <samuelnoye35@gmail.com>
2025-03-22 04:08:18 +0100

    update

--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -3,14 +3,15 @@ const admin = require("firebase-admin");
 // Load Firebase service account key (download from Firebase console)
 const serviceAccount = require("../chopnow-5110f-firebase-adminsdk-okmba-1674c2d0f5.json");
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-      credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      }),
-  });
-}
+require("dotenv").config();
 
+admin.initializeApp({
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
+            ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+            : undefined,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+});
 module.exports = admin;
